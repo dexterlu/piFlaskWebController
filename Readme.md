ccfdb4321da14776619fb22bc2fb1e065e7eb061
dexterlu <dexter.yi@gmail.com>
2017-08-08 16:51:06 +0800

    improvement

--- Readme.md
+++ Readme.md
@@ -4,13 +4,20 @@ Web Simple Button control Rpi3 GPIO controller
 ### Requirement
 - pre-install requirements
 ```
+sudo pip3 install --upgrade pip setuptools
 sudo pip install -r requirements.txt
 ```
 
-### Runing
+### Runing on Raspberry Pi3
+```
+python3 main_pi.py
+```
+
+### Runing on Ubuntu
 ```
 python3 main.py
 ```
+
 and then visit:
 ```
 http://[your Rpi3's IP Address]:5678
--- main_pi.py
+++ main_pi.py
@@ -1,6 +1,7 @@
 from gevent import monkey
 monkey.patch_all()
 
+import RPi.GPIO as GPIO
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.socketio import SocketIO
 
@@ -8,6 +9,10 @@ app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
 
+pin_out_1 = 6 #18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_out_1, GPIO.OUT)
+GPIO.output(pin_out_1, True)
 
 @app.route('/')
 def main():
@@ -16,7 +21,7 @@ def main():
 @socketio.on('connect', namespace='/socket')
 def ws_conn():
     data = {
-        #'button_1': GPIO.input(pin_out_1)
+        'button_1': GPIO.input(pin_out_1)
     }
     socketio.emit('status_btn', data, namespace='/socket')
 
@@ -24,11 +29,10 @@ def ws_conn():
 def ws_btn(message):
     button = message.get('button')
     if button==1:
-        #GPIO.output(pin_out_1, not GPIO.input(pin_out_1))
-        #data = {
-        #    'button_1': GPIO.input(pin_out_1)
-        #}
-        data = 1
+        GPIO.output(pin_out_1, not GPIO.input(pin_out_1))
+        data = {
+            'button_1': GPIO.input(pin_out_1)
+        }
         socketio.emit('status_btn', data, namespace='/socket')
 
 if __name__ == '__main__':
